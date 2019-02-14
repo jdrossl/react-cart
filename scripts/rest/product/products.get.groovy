49e80bbd6835a9f40a8c569279dd1b37f59f6b7c
admin admin <evaladmin@example.com>
2019-02-14 17:44:21 -0500

    User admin wrote content /scripts/rest/product/products.get.groovy

--- scripts/rest/product/products.get.groovy
+++ scripts/rest/product/products.get.groovy
@@ -24,6 +24,7 @@ executedQuery.response.documents.eachWithIndex { document, idx ->
                     price:          getPrice(document),        // potentially get the price from external system
                     installments:   getInventory(document),    // potentially get inventory from external system
                     isFreeShipping: document.freeShipping,
+                    endorsedBy:     (document.endorsedBy) ? document.endorsedBy : "Hello World",
                     availableSizes: document["sizes.item.key"],
                     currencyId:     "USD",  // hard code USD for now
                     currencyFormat: "\$",   // hard code currency format for now
